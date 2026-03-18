d03eb083d8392fc3f4401248bc90cb03c3263b92
ryuma04 <168087662+ryuma04@users.noreply.github.com>
2026-03-19 01:08:08 +0530

    Changing the backend domain after deployment

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'}/:path*`,
       },
     ];
   },
