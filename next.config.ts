27439b49fa71a9661408165d986f21ca86389d99
ryuma04 <168087662+ryuma04@users.noreply.github.com>
2026-03-19 01:19:27 +0530

    second changing the backend domain after deployment

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'}/:path*`,
+        destination: 'https://ideora-backend.onrender.com/api/:path*',
       },
     ];
   },
